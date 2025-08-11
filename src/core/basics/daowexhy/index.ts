b32f905ce72af5e801ce396a67ee178de1501858
Sergej Sintschilin <s.sintschilin.extern@senec.com>
2025-08-11 12:47:44 +0200

    +

--- src/core/basics/daowexhy/index.ts
+++ src/core/basics/daowexhy/index.ts
@@ -10,6 +10,10 @@ import {isSignal, signal, Signal} from '@angular/core';
  */
 export type MaybeSignal<T> = T | Signal<T>;
 
+export type UnwrapSignal<T> = T extends Signal<infer R> ? R : T;
+
+export type WrapSignal<T> = T extends Signal<?> ? T : Signal<T>;
+
 /**
  * Bla bla bla
  *
@@ -21,19 +25,21 @@ export function unwrapSignal<const T>(
   v: MaybeSignal<T>,
 ): T;
 
-export const unwrapSignal = (v) => (isSignal(v) ? v() : v);
+export function unwrapSignal(v) {
+  return isSignal(v) ? v() : v;
+}
 
 /**
- * Wraps a value in a Signal. If the input is already a Signal, it is returned as is. Otherwise,
- * it creates a new Signal wrapping the value.
+ * Bla bla bla.
  *
- * @template T The type of the value to be wrapped in a Signal.
- * @param {MaybeSignal<T>} v The value or Signal to wrap.
- * @returns {Signal<T>} A Signal wrapping the value.
+ * @template T Bla bla bla.
+ * @param {MaybeSignal<T>} v Bla bla bla.
+ * @returns {Signal<T>} Bla bla bla.
  */
 export function wrapSignal<const T>(
-  //
   v: MaybeSignal<T>,
 ): Signal<T>;
 
-export const wrapSignal = (v) => (isSignal(v) ? v : signal(v));
+export function wrapSignal(v) {
+  return isSignal(v) ? v : signal(v);
+}
