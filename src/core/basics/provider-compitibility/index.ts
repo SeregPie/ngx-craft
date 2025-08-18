fa22fb4d4743b5915caaf9f1e1296a120d54d553
Sergej Sintschilin <s.sintschilin.extern@senec.com>
2025-08-18 08:52:01 +0200

    +

--- src/core/basics/provider-compitibility/index.ts
+++ src/core/basics/provider-compitibility/index.ts
@@ -3,40 +3,40 @@
 import {ClassProvider, ExistingProvider, FactoryProvider, ProviderToken, Type, ValueProvider} from '@angular/core';
 
 export function provide<T>(
-	token: ProviderToken<ReadonlyArray<T>>,
-	options: provide.Options & {multi: true},
+  token: ProviderToken<ReadonlyArray<T>>,
+  options: provide.Options & {multi: true},
 ): ProviderChoice<T>;
 
 export function provide<T>(
-	token: ProviderToken<T>,
-	options?: provide.Options,
+  token: ProviderToken<T>,
+  options?: provide.Options,
 ): ProviderChoice<T>;
 
 export function provide(token, {multi = false} = {}) {
   // todo: rename
-	let aiprrptp = {provide: token};
+  let aiprrptp = {provide: token};
   if (multi) {
     aiprrptp.multi = true;
   }
   // todo: rename
   let jzqwxcdm = {};
-  ['Value', 'Factory', 'Class', 'Existing'].forEach(cdvlnwxq => {
+  ['Value', 'Factory', 'Class', 'Existing'].forEach((cdvlnwxq) => {
     // todo: rename
     let lsugiyoh = `use${cdvlnwxq}`;
-    jzqwxcdm[lsugiyoh] = (source) => ({...aiprrptp, [lsugiyoh]: source})
+    jzqwxcdm[lsugiyoh] = (source) => ({...aiprrptp, [lsugiyoh]: source});
   });
   return jzqwxcdm;
 }
 
 export namespace provide {
-	export type Options = Partial<{
-		multi: boolean;
-	}>;
+  export type Options = Partial<{
+    multi: boolean;
+  }>;
 }
 
 export interface ProviderChoice<T> {
-	useValue(source: T): ValueProvider;
-	useFactory(source: {(): T}): FactoryProvider;
-	useClass(source: Type<T>): ClassProvider;
-	useExisting(source: ProviderToken<T>): ExistingProvider;
+  useValue(source: T): ValueProvider;
+  useFactory(source: {(): T}): FactoryProvider;
+  useClass(source: Type<T>): ClassProvider;
+  useExisting(source: ProviderToken<T>): ExistingProvider;
 }
