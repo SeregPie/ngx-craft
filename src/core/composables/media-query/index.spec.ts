0ad6b4fe34c30a923c3914b539e1b6d575b10f82
Sergej Sintschilin <seregpie@gmail.com>
2025-08-20 21:53:28 +0200

    +

---
+++ src/core/composables/media-query/index.spec.ts
@@ -0,0 +1,18 @@
+import {describe, expect, it, jest} from '@jest/globals';
+
+import { DOCUMENT, inject } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+describe('useMediaQuery', () => {
+  it('...', async () => {
+    const document = TestBed.inject(DOCUMENT);
+    const window = document.defaultView;
+    if (window != null) {
+      console.log(window.matchMedia, window.matchMedia('(prefers-color-scheme: dark)'), window.MediaQueryList);
+    }
+   
+    
+
+
+  });
+});
