8953516bf9953fcda693963c7a496c778efeddfb
Sergej Sintschilin <seregpie@gmail.com>
2025-08-27 17:01:59 +0200

    +

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,45 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import json from '@eslint/json';
-import markdown from '@eslint/markdown';
-import {defineConfig} from 'eslint/config';
-import stylistic from '@stylistic/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import {defineConfig} from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export const globJS = '*.?([cm])js';
-export const globJSX = '*.?([cm])jsx';
+export const globJS = "*.?([cm])js";
+export const globJSX = "*.?([cm])jsx";
 
-export const globTS = '*.?([cm])ts';
-export const globTSX = '*.?([cm])tsx';
+export const globTS = "*.?([cm])ts";
+export const globTSX = "*.?([cm])tsx";
 
-export const globHTML = '*.htm?(l)';
+export const globHTML = "*.htm?(l)";
 
-export const globSVG = '*.svg';
+export const globSVG = "*.svg";
 
-export const globCSS = '*.css';
-export const globPostCSS = '*.{p,post}css';
-export const globLESS = '*.less';
-export const globSCSS = '*.scss';
+export const globCSS = "*.css";
+export const globPostCSS = "*.{p,post}css";
+export const globLESS = "*.less";
+export const globSCSS = "*.scss";
 
-export const globAngularTemplate = '*.ng.html';
-export const globVue = '*.vue';
-export const globSvelte = '*.svelte';
+export const globAngularTemplate = "*.ng.html";
+export const globVue = "*.vue";
+export const globSvelte = "*.svelte";
 
-export const globJSON = '*.json';
-export const globJSON5 = '*.json5';
-export const globJSONC = '*.jsonc';
+export const globJSON = "*.json";
+export const globJSON5 = "*.json5";
+export const globJSONC = "*.jsonc";
 
-export const globYAML = '*.y?(a)ml';
-export const globTOML = '*.toml';
-export const globXML = '*.xml';
+export const globYAML = "*.y?(a)ml";
+export const globTOML = "*.toml";
+export const globXML = "*.xml";
 
-export const globGraphQL = '*.{g,graph}ql';
+export const globGraphQL = "*.{g,graph}ql";
 
-export const globMarkdown = '*.md';
+export const globMarkdown = "*.md";
 
 export function uhhxmpvq(name) {
-  const i = name.lastIndexOf('/');
+  const i = name.lastIndexOf("/");
   return i < 0 ? name : name.slice(i + 1);
 }
 
@@ -59,6 +59,10 @@ export function zskcshyg(prefix, rules) {
   return result;
 }
 
+export function defineSeregPieConfig() {
+  let severity = "error";
+}
+
 export default defineConfig([
   /*
   await (async () => {
@@ -73,13 +77,14 @@ export default defineConfig([
   })(),
   */
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: {
       parser: tsParser,
     },
     ...(() => {
       const config = stylistic.configs.customize({
         semi: true,
+        quotes: "double",
         arrowParens: true,
         jsx: false,
       });
@@ -88,9 +93,9 @@ export default defineConfig([
         plugins: {
           ts: stylistic,
         },
-        rules: zskcshyg('ts', {
+        rules: zskcshyg("ts", {
           ...rules,
-          'object-curly-spacing': ['error', 'never'],
+          "object-curly-spacing": ["error", "never"],
         }),
       };
     })(),
