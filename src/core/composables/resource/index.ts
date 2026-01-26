23f9f631610812ee51e87ea243891981e4e0add7
Sergej Sintschilin <seregpie@gmail.com>
2026-01-26 14:05:10 +0100

    +

--- src/core/composables/resource/index.ts
+++ src/core/composables/resource/index.ts
@@ -7,8 +7,68 @@ export function useResource<const T>(
   fn: {(): Promise<T>},
 ): Resource<T>;
 
-export function useResource(fn) {
-  // todo
+import {computed, effect, signal, untracked} from '@angular/core';
+import {PromiseWithResolvers, withResolvers} from 'radashi';
+
+export function useResource<T>(fn: {(): Promise<T>}): Signal<Promise<T>> & {
+  pending: Signal<boolean>;
+  value: Signal<undefined | T>;
+  error: Signal<unknown>;
+  trigger: {(): void};
+} {
+  const pendingSignal = signal<boolean>(false);
+  const valueSignal = signal<undefined | T>(undefined);
+  const errorSignal = signal<unknown>(undefined);
+  let controller: undefined | AbortController = undefined;
+  let qnsmqtbn: undefined | PromiseWithResolvers<T> = undefined;
+  const kissngyz = signal<unknown>(undefined);
+  const ogvhahqw = computed(async () => {
+    kissngyz();
+    if (controller != null) {
+      controller.abort();
+    }
+    controller = new AbortController();
+    const {signal} = controller;
+    if (qnsmqtbn == null) {
+      qnsmqtbn = withResolvers();
+    }
+    const {promise, resolve, reject} = qnsmqtbn;
+    (async () => {
+      untracked(() => {
+        pendingSignal.set(true);
+      });
+      try {
+        const value = await fn();
+        if (!signal.aborted) {
+          resolve(value);
+          valueSignal.set(value);
+          errorSignal.set(undefined);
+        }
+      } catch (error) {
+        if (!signal.aborted) {
+          reject(error);
+          errorSignal.set(error);
+        }
+      } finally {
+        if (!signal.aborted) {
+          controller = undefined;
+          qnsmqtbn = undefined;
+          pendingSignal.set(false);
+        }
+      }
+    })();
+    return promise;
+  });
+  effect(() => ogvhahqw());
+  const trigger = () => {
+    kissngyz.set({});
+  };
+  return Object.assign(ogvhahqw, {
+    pending: pendingSignal,
+    value: valueSignal,
+    error: errorSignal,
+    trigger,
+  });
 }
 
 export type Resource = Signal<Promise<T>> & {
